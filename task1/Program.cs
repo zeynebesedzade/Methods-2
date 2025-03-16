8ff2e8e48565d84f650bf6660a01e3c27b2bdd28
zeynebesedzade <zeynabia-bb104@code.edu.az>
2025-03-16 20:29:32 +0400

    min method

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+class Program
+{
+    public static int Min(int[] array)
+    {
+        if (array.Length == 0) return 0;
+
+        int min = array[0];
+        for (int i = 1; i < array.Length; i++)
+            if (array[i] < min)
+                min = array[i];
+
+        return min;
+    }
+
+    static void Main()
+    {
+        int[] numbers = { 5, 2, 8, 1, 9, 3 };
+        Console.WriteLine("En kiçik element: " + Min(numbers));
+    }
+}
