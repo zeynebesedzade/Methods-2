25b0a06f6d14a0cff03937a07c582c704e5b4817
zeynebesedzade <zeynabia-bb104@code.edu.az>
2025-03-16 21:17:42 +0400

    saheler

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,23 +1,65 @@
 ﻿
+//using System;
+
+//class Program
+//{
+//    public static int Min(int[] array)
+//    {
+//        if (array.Length == 0) return 0;
+
+//        int min = array[0];
+//        for (int i = 1; i < array.Length; i++)
+//            if (array[i] < min)
+//                min = array[i];
+
+//        return min;
+//    }
+
+//    static void Main()
+//    {
+//        int[] numbers = { 5, 2, 8, 1, 9, 3 };
+//        Console.WriteLine("En kiçik element: " + Min(numbers));
+//    }
+//}
+
+
 using System;
 
 class Program
 {
-    public static int Min(int[] array)
+    //cevrenin sahesi
+    public static int Area(int r)
     {
-        if (array.Length == 0) return 0;
+        int p = 3;
+        return p * r * r;
+    }
 
-        int min = array[0];
-        for (int i = 1; i < array.Length; i++)
-            if (array[i] < min)
-                min = array[i];
+    // Düzbucaqlının sahesi 
+    public static int Area(int a, int b)
+    {
+        return a * b;
+    }
 
-        return min;
+    // Düzbucaqlı paralelpipedin tam səthinin sahesi 
+    public static int Area(int a, int b, int c)
+    {
+        return 2 * (a * b + a * c + b * c);
+    }
+
+    // Üçbucaqlının daxiline çekilmiş çevrenin sahesi 
+    public static int Area(int a, int b, int c, int r)
+    {
+        int p = (a + b + c) / 2;
+        return p * r;
     }
 
     static void Main()
     {
-        int[] numbers = { 5, 2, 8, 1, 9, 3 };
-        Console.WriteLine("En kiçik element: " + Min(numbers));
+        Console.WriteLine("Çevrenin sahesi: " + Area(8));
+        Console.WriteLine("Düzbucaqlının sahesi: " + Area(4, 6));
+        Console.WriteLine("Düzbucaqlı paralelpipedin tam sethinin sahesi: " + Area(3, 5, 7));
+        Console.WriteLine("Üçbucaqlının daxiline çekilmiş çevrenin sahesi: " + Area(3, 4, 8, 2));
     }
 }
+
+
--- task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff2e8e48565d84f650bf6660a01e3c27b2bdd28")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
